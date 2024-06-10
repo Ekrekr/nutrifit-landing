dfc3052f64ec9c2f57c628395912390dec55c3f6
Elias Kassell <eliaskassell@gmail.com>
2024-06-10 21:18:15 +0100

    Add a forms page

---
+++ app/forms/page.tsx
@@ -0,0 +1,17 @@
+export default function Forms() {
+  return (
+    <body className="p-20">
+      <h1>Forms:</h1>
+      <ul>
+        <li>
+          <a
+            href="https://forms.gle/137N78TaTQQ6iQnF6"
+            className="text-blue-600"
+          >
+            Request account deletion
+          </a>
+        </li>
+      </ul>
+    </body>
+  );
+}
